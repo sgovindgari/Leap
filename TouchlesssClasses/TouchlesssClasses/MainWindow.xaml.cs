4ab4c1095673cb4d946939452412a43123f21942
daleprc <dale.prc@gmail.com>
2013-08-17 17:15:44 -0700

    first ui push

--- TouchlesssClasses/TouchlesssClasses/MainWindow.xaml.cs
+++ TouchlesssClasses/TouchlesssClasses/MainWindow.xaml.cs
@@ -12,16 +12,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Leap;
 
-namespace TouchlesssClasses
+namespace Touchless2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel viewModel;
+
         public MainWindow()
         {
+            this.viewModel = new MainWindowViewModel();
+            this.DataContext = this.viewModel;
             InitializeComponent();
         }
     }
---
+++ TouchlesssClasses/TouchlesssClasses/MainWindowViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Touchless2
+{
+    public enum Mode
+    {
+        Desktop,
+        Presentation
+    }
+
+    public enum Option
+    {
+        Touch,
+        Gestures
+    }
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private Mode mode;
+        public Mode Mode
+        {
+            get { return this.mode; }
+            set
+            {
+                if (this.mode != value)
+                {
+                    this.mode = value;
+                    this.OnPropertyChanged("Mode");
+                }
+            }
+        }
+
+        private Option option;
+        public Option Option
+        {
+            get { return this.option; }
+            set
+            {
+                if (this.option != value)
+                {
+                    this.option = value;
+                    this.OnPropertyChanged("Option");
+                    this.OnPropertyChanged("TouchPanelVisibility");
+                    this.OnPropertyChanged("GesturesPanelVisibility");
+                }
+            }
+        }
+
+        public Visibility TouchPanelVisibility
+        {
+            get { return this.option == Option.Touch ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
+        public Visibility GesturesPanelVisibility
+        {
+            get { return this.option == Option.Gestures ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
+        public ObservableCollection<ScreenViewModel> Screens
+        {
+            get;
+            private set;
+        }
+
+        public MainWindowViewModel()
+        {
+            this.Screens = new ObservableCollection<ScreenViewModel>();
+            this.LoadData();
+        }
+
+        private void LoadData()
+        {
+            this.mode = Mode.Desktop;
+            this.option = Option.Touch;
+            this.Screens.Clear();
+            foreach (var screen in Screen.AllScreens)
+            {
+                this.Screens.Add(new ScreenViewModel(screen));
+            }
+        }
+    }
+}
