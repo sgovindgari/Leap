3c9d805e16064f411bfaee058634d71fe2485c7d
U-sakurai\Scarlet <Scarlet@sakurai.(none)>
2013-08-17 18:59:08 -0700

    aaa

--- TouchlesssClasses/TouchlesssClasses/MainWindow.xaml.cs
+++ TouchlesssClasses/TouchlesssClasses/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Leap;
+using System.Runtime.InteropServices;
 
 namespace Touchless2
 {
@@ -22,12 +23,31 @@ namespace Touchless2
     public partial class MainWindow : Window
     {
         private MainWindowViewModel viewModel;
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+
+        [DllImport(@"user32.dll")]
+        private static extern IntPtr FindWindow(string lp1, string lp2);
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 
         public MainWindow()
         {
             this.viewModel = new MainWindowViewModel();
             this.DataContext = this.viewModel;
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IntPtr hWnd = FindWindow("Notepad", "Untitled - Notepad");
+            if (!hWnd.Equals(IntPtr.Zero))
+            {
+                // SW_SHOWMAXIMIZED to maximize the window
+                // SW_SHOWMINIMIZED to minimize the window
+                // SW_SHOWNORMAL to make the window be normal size
+                ShowWindowAsync(hWnd, SW_SHOWMAXIMIZED);
+            }
         }
     }
 }
